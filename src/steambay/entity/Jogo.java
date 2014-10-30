abd317ee6e77a63038be9b6cbef67b888a8fa364
Gabriel Carreira Lovetro <gabe_man2001@yahoo.com.br>
2014-10-30 17:06:17 -0200

    Atributos Jogo.java.

--- src/steambay/entity/Jogo.java
+++ src/steambay/entity/Jogo.java
@@ -10,5 +10,69 @@ package steambay.entity;
  * @author Daniel, Thales e Gabriel
  */
 public class Jogo {
+    private String nome;
+    private boolean tipo;
+    private int qtde;
+    private float tamanho;
+    private float preco;
+    private String especificacao;
+    private String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public float getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(float tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getEspecificacao() {
+        return especificacao;
+    }
+
+    public void setEspecificacao(String especificacao) {
+        this.especificacao = especificacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
     
 }
