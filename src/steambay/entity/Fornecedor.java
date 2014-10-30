791d06820ee69629c0e4cdfa6c3bdce1c693a0a7
Thales Martins <linkthales@hotmail.com>
2014-10-30 17:10:45 -0200

    Atributos Fornecedor

--- src/steambay/entity/Fornecedor.java
+++ src/steambay/entity/Fornecedor.java
@@ -10,5 +10,51 @@ package steambay.entity;
  * @author Daniel, Thales e Gabriel
  */
 public class Fornecedor {
+    String nome;
+    String nome_fantasia;
+    String cnpj;
+    String telefone;
+    String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome_fantasia() {
+        return nome_fantasia;
+    }
+
+    public void setNome_fantasia(String nome_fantasia) {
+        this.nome_fantasia = nome_fantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
     
 }
