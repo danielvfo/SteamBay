ab4557354cf6f05e62c42bdedb2288bfdf4a2c68
danielvfo <danielvf@outlook.com>
2014-10-30 16:42:31 -0200

    Classes de inteface.

--- src/steambay/ui/CadastrarProdutoUI.java
+++ src/steambay/ui/CadastrarProdutoUI.java
@@ -9,12 +9,12 @@ package steambay.ui;
  *
  * @author Daniel
  */
-public class GerenciarCarrinhoUI extends javax.swing.JFrame {
+public class CadastrarProdutoUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form GerenciarCarrinhoUI
+     * Creates new form CadastrarProdutoUI
      */
-    public GerenciarCarrinhoUI() {
+    public CadastrarProdutoUI() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class GerenciarCarrinhoUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GerenciarCarrinhoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarProdutoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GerenciarCarrinhoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarProdutoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GerenciarCarrinhoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarProdutoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GerenciarCarrinhoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastrarProdutoUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GerenciarCarrinhoUI().setVisible(true);
+                new CadastrarProdutoUI().setVisible(true);
             }
         });
     }
